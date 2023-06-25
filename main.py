460b3f598fd9080f729049fa985d63cb11c557b5
Guilherme Santos <guilhermesantos.adv@protonmail.com>
2023-06-24 22:49:29 -0300

    Implementado banco de dados e plotagem de gráfico

--- main.py
+++ main.py
@@ -1,78 +1,58 @@
 from playwright.sync_api import Playwright, sync_playwright
-import json
+import matplotlib.pyplot as plt
 import sqlite3
 import datetime
 
-
-jsonFile = 'produtos.json'
-
-conn = sqlite3.connect('precos.db')
-cursor = conn.cursor()
-
-conn.execute('''CREATE TABLE IF NOT EXISTS precos
-                (produto TEXT, horario TIMESTAMP, preco REAL)''')
-
-def save_to_DB(produto, preco):
-    conn = sqlite3.connect('precos.db')
+# Função para salvar dados no banco de dados
+def save_to_DB(nome, preço_atual):
+    conn = sqlite3.connect('banco.db')
     horario = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
-    conn.execute('INSERT INTO precos VALUES (?, ?, ?)', (produto, horario, preco))
+    conn.execute('INSERT INTO dados VALUES (?, ?, ?)', (str(nome), horario, preço_atual))
     conn.commit()
     conn.close()
 
+# Função para obter os dados do banco de dados
+def obter_dados():
+    conn = sqlite3.connect('banco.db')
+    cursor = conn.cursor()
+    cursor.execute('SELECT produto, horario, precos FROM dados')
+    dados = cursor.fetchall()
+    conn.close()
+    return dados
+
+# Criação do banco de dados
+conn = sqlite3.connect('banco.db')
+cursor = conn.cursor()
+conn.execute('''CREATE TABLE IF NOT EXISTS dados
+                (produto TEXT, horario TIMESTAMP, precos REAL)''')
+
+# Etapa de raspagem de dados
 with sync_playwright() as sync_p:
     browser = sync_p.firefox.launch()
     page = browser.new_page()
 
     url = input('Digite o link do produto: ')
 
-    # Verifica os produtos já cadastrados, caso exista algum
-    try:
-        with open(jsonFile, 'r') as f:
-            produtos = json.load(f)
-    except FileExistsError:
-        produtos = []
+    page.goto(url)
 
-    # Verifica as informações do produto
-    produto_cadastrado = None
-    for produto in produtos:
-        if produto['url'] == url:
-            produto_cadastrado = produto
-            break
+    elemento_preço = page.query_selector('meta[itemprop="price"]')
+    preço_atual = f"R$ {elemento_preço.get_attribute('content')}"
 
-    # Verifica se houve alteração no preço do produto
-    if produto_cadastrado:
-        page.goto(url)
-        elemento_preço = page.query_selector('meta[itemprop="price"]')
-        preço_atual = f"R$ {elemento_preço.get_attribute('content')}"
+    elemento_nome = page.query_selector('h1.ui-pdp-title')
+    nome = elemento_nome.inner_text().strip()
 
-        if preço_atual != produto_cadastrado['preco']:
-            # Atualiza o preço do produto
-            produto_cadastrado['preco'] = preço_atual
-            with open(jsonFile, 'w') as f:
-                json.dump(produtos, f)
-                print('Atualização do preço realizada com sucesso!!!')
+    save_to_DB(nome, preço_atual)
 
-        else:
-            print('Não houve alteração no preço do produto')
+dados = obter_dados()
+produtos, horarios, precos = zip(*dados)
+horarios = [datetime.datetime.strptime(h, '%Y-%m-%d %H:%M:%S') for h in horarios]
 
-    else:
-        # Produto novo
-        page.goto(url)
-
-        elemento_preço = page.query_selector('meta[itemprop="price"]')
-        preço_atual = f"R$ {elemento_preço.get_attribute('content')}"
-
-        elemento_nome = page.query_selector('h1.ui-pdp-title')
-        nome = elemento_nome.inner_text().strip()
-
-        produto_novo = {'url': url, 'nome': nome, 'preco': preço_atual}
-        produtos.append(produto_novo)
-
-        with open(jsonFile, 'w') as f:
-            json.dump(produtos, f)
-            print('Produto adicionado com sucesso!!!')
-
-    browser.close()
+# Plotar o gráfico
+plt.plot(horarios, precos, marker='o')
+plt.xlabel('Horário')
+plt.ylabel('Preço')
+plt.title('Variação de Preços')
+plt.xticks(rotation=45)
+plt.show()
 
 conn.close()
-
